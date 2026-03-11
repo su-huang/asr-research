0efaef700ee00527501d28713eca063ffcb8e5ba
Su <suhuang2007@gmail.com>
2026-03-11 10:11:25 -0400

    read and parse csv with dictreader

--- cpd_audio/zeroshot_on_testset/normalization.py
+++ cpd_audio/zeroshot_on_testset/normalization.py
@@ -1,5 +1,6 @@
 # Whisper output normalization and updated dtl
 import os
+import csv 
 
 def replace_question_marks(filename, bad_word_fixes):
     try:
@@ -22,13 +23,24 @@ def replace_question_marks(filename, bad_word_fixes):
 def get_bad_word_fixes():
     # Load file with "fixed" tokens
     bad_words_fixed = {}
-    with open('/secure/fs00/afield6/police/shuan148/bpc-cpdForAF.csv','r', encoding='latin1') as f:
-        next(f)
-        for line in f:
-            if len(line.split(','))>1:
-                orig, fixed = line.split(',')
-                if fixed.replace('\n','') != '':
-                    bad_words_fixed[orig] = fixed.replace('\n','')
+    # with open('/secure/fs00/afield6/police/shuan148/bpc-cpdForAF.csv','r', encoding='latin1') as f:
+    #     next(f)
+    #     for line in f:
+    #         if len(line.split(','))>1:
+    #             orig, fixed = line.split(',')
+    #             if fixed.replace('\n','') != '':
+    #                 bad_words_fixed[orig] = fixed.replace('\n','')
+
+    path = '/secure/fs00/afield6/police/shuan148/bpc-cpdForAF.csv'
+    
+    with open(path, 'r', encoding='latin1') as f:
+        # DictReader uses header names
+        reader = csv.DictReader(f)        
+        for row in reader:
+            orig = row.get('transcription_raw', '').strip().upper()
+            fixed = row.get('transcription_final', '').strip()            
+            if orig and fixed:
+                bad_words_fixed[orig] = fixed
 
     # Add known confusions (just to be sure)
     bad_words_fixed['FOURTY'] = 'FORTY'
