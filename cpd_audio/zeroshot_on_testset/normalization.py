3f587b8a585382c6173e83e2ed354b921e0a6740
Su <suhuang2007@gmail.com>
2026-03-24 08:33:33 -0400

    fix csv file parsing

--- cpd_audio/zeroshot_on_testset/normalization.py
+++ cpd_audio/zeroshot_on_testset/normalization.py
@@ -31,16 +31,15 @@ def get_bad_word_fixes():
     #             if fixed.replace('\n','') != '':
     #                 bad_words_fixed[orig] = fixed.replace('\n','')
 
-    path = '/secure/fs00/afield6/police/shuan148/bpc-cpdForAF.csv'
+    path = '/secure/fs00/afield6/police/shuan148/bad_words_fixed.csv'
     
-    with open(path, 'r', encoding='latin1') as f:
-        # DictReader uses header names
-        reader = csv.DictReader(f)        
-        for row in reader:
-            orig = row.get('transcription_raw', '').strip().upper()
-            fixed = row.get('transcription_final', '').strip()            
-            if orig and fixed:
-                bad_words_fixed[orig] = fixed
+    with open(path,'r', encoding='latin1') as f:
+        next(f)
+        for line in f:
+            if len(line.split(','))>1:
+                orig, fixed = line.split(',')
+                if fixed.replace('\n','') != '':
+                    bad_words_fixed[orig] = fixed.replace('\n','')
 
     # Add known confusions (just to be sure)
     bad_words_fixed['FOURTY'] = 'FORTY'
